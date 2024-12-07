f690e6a4841fa6422eb692a3fbff454c07adecc8
aapetukhov <andreybsongs@gmail.com>
2024-12-08 01:17:45 +0300

    add collate

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,5 +1,6 @@
-import torch
+from torch.nn.utils.rnn import pad_sequence
 
+PAD_CONST = -11.51292
 
 def collate_fn(dataset_items: list[dict]):
     """
@@ -14,12 +15,17 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    result_batch = {}
+    audios = [audio for item in dataset_items for audio in item['audio']]
+    audios = pad_sequence(audios, batch_first=True)
 
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
-    )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
+    spectrograms = [
+        spectrogram.transpose(1, 2) for item in dataset_items for spectrogram in item['spectrogram']
+    ]
+    spectrograms = pad_sequence(spectrograms, batch_first=True, padding_value=PAD_CONST).transpose(1, 2)
+    texts = sum((item['text'] for item in dataset_items), [])
 
-    return result_batch
+    return {
+        'audio': audios,
+        'spectrogram': spectrograms,
+        'text': texts
+    }
