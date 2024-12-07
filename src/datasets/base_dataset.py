0e1598bce973ef8f66867c769c6d4bdaf4ab43eb
aapetukhov <andreybsongs@gmail.com>
2024-12-08 00:10:26 +0300

    add base dataset logic

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -1,8 +1,10 @@
 import logging
 import random
+import torchaudio
 from typing import List
 
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
@@ -18,7 +20,14 @@ class BaseDataset(Dataset):
     """
 
     def __init__(
-        self, index, limit=None, shuffle_index=False, instance_transforms=None
+        self,
+        index,
+        target_sr: int = 22050,
+        max_len: int = 8192 * 2,
+        max_audio_length: int = None,
+        limit = None,
+        shuffle_index = False,
+        instance_transforms = None,
     ):
         """
         Args:
@@ -38,7 +47,9 @@ class BaseDataset(Dataset):
         index = self._shuffle_and_limit_index(index, limit, shuffle_index)
         self._index: List[dict] = index
 
+        self.target_sr = target_sr
         self.instance_transforms = instance_transforms
+        self.max_len = max_len
 
     def __getitem__(self, ind):
         """
@@ -56,12 +67,20 @@ class BaseDataset(Dataset):
                 (a single dataset element).
         """
         data_dict = self._index[ind]
-        data_path = data_dict["path"]
-        data_object = self.load_object(data_path)
-        data_label = data_dict["label"]
+        audio_path = data_dict["path"]
+        audio = self.load_audio(audio_path)
+        text = data_dict["text"]
+        spectrogram = self.get_spectrogram(audio)
 
-        instance_data = {"data_object": data_object, "labels": data_label}
-        instance_data = self.preprocess_data(instance_data)
+        instance_data = {
+            "text": text,
+            "audio_path": audio_path,
+            "audio": audio,
+            "spectrogram": spectrogram,
+        }
+        for key, value in instance_data.items():
+            if isinstance(value, torch.Tensor):
+                assert value.device.type == "cpu", f"{key} is on {value.device.type}"
 
         return instance_data
 
@@ -71,42 +90,26 @@ class BaseDataset(Dataset):
         """
         return len(self._index)
 
-    def load_object(self, path):
-        """
-        Load object from disk.
+    def load_audio(self, path):
+        audio_tensor, sr = torchaudio.load(path)
+        audio_tensor = audio_tensor[:1]
+        if sr != self.target_sr:
+            audio_tensor = torchaudio.functional.resample(audio_tensor, sr, self.target_sr)
+        audio_tensor = self._process_audio(audio_tensor, self.max_len)
+        return audio_tensor
 
-        Args:
-            path (str): path to the object.
-        Returns:
-            data_object (Tensor):
-        """
-        data_object = torch.load(path)
-        return data_object
-
-    def preprocess_data(self, instance_data):
-        """
-        Preprocess data with instance transforms.
-
-        Each tensor in a dict undergoes its own transform defined by the key.
-
-        Args:
-            instance_data (dict): dict, containing instance
-                (a single dataset element).
-        Returns:
-            instance_data (dict): dict, containing instance
-                (a single dataset element) (possibly transformed via
-                instance transform).
-        """
-        if self.instance_transforms is not None:
-            for transform_name in self.instance_transforms.keys():
-                instance_data[transform_name] = self.instance_transforms[
-                    transform_name
-                ](instance_data[transform_name])
-        return instance_data
+    @staticmethod
+    def _process_audio(audio_tensor, length):
+        # cutting or padding to the required length
+        if audio_tensor.shape[1] > length:
+            start = random.randint(0, audio_tensor.shape[1] - length)
+            return audio_tensor[:, start:start + length]
+        return torch.nn.functional.pad(audio_tensor, (0, length - audio_tensor.shape[1]))
 
     @staticmethod
     def _filter_records_from_dataset(
         index: list,
+        max_audio_length: int = None,
     ) -> list:
         """
         Filter some of the elements from the dataset depending on
@@ -124,8 +127,72 @@ class BaseDataset(Dataset):
                 the dataset that satisfied the condition. The dict has
                 required metadata information, such as label and object path.
         """
-        # Filter logic
-        pass
+        initial_size = len(index)
+        if max_audio_length is not None:
+            exceeds_audio_length = (
+                np.array([el["audio_len"] for el in index]) >= max_audio_length
+            )
+            _total = exceeds_audio_length.sum()
+            logger.info(
+                f"{_total} ({_total / initial_size:.1%}) records are longer then "
+                f"{max_audio_length} seconds. Excluding them."
+            )
+        else:
+            exceeds_audio_length = False
+
+        initial_size = len(index)
+
+        records_to_filter = exceeds_audio_length
+
+        if records_to_filter is not False and records_to_filter.any():
+            _total = records_to_filter.sum()
+            index = [el for el, exclude in zip(index, records_to_filter) if not exclude]
+            logger.info(
+                f"Filtered {_total} ({_total / initial_size:.1%}) records  from dataset"
+            )
+
+        return index
+
+    def get_spectrogram(self, audio):
+        """
+        Special instance transform with a special key to
+        get spectrogram from audio.
+
+        Args:
+            audio (Tensor): original audio.
+        Returns:
+            spectrogram (Tensor): spectrogram for the audio.
+        """
+        return self.instance_transforms["get_spectrogram"](audio)
+
+    def preprocess_data(self, instance_data, special_keys=["get_spectrogram"], single_key=None):
+        """
+        Preprocess data with instance transforms.
+
+        Each tensor in a dict undergoes its own transform defined by the key.
+
+        Args:
+            instance_data (dict): dict, containing instance
+                (a single dataset element).
+            single_key: optional[str]: if set modifies only this key
+        Returns:
+            instance_data (dict): dict, containing instance
+                (a single dataset element) (possibly transformed via
+                instance transform).
+        """
+        if self.instance_transforms is None:
+            return instance_data
+
+        if single_key is not None:
+            if single_key in self.instance_transforms:  # eg train mode
+                instance_data[single_key] = self.instance_transforms[single_key](instance_data[single_key])
+            return instance_data
+
+        for transform_name in self.instance_transforms.keys():
+            if transform_name in special_keys:
+                continue  # skip special key
+            instance_data[transform_name] = self.instance_transforms[transform_name](instance_data[transform_name])
+        return instance_data
 
     @staticmethod
     def _assert_index_is_valid(index):
