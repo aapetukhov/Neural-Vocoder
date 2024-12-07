fca084b723d4d0a970d443a4ac9b68a9a2539521
aapetukhov <andreybsongs@gmail.com>
2024-12-08 02:33:10 +0300

    add datasets for audio and text

---
+++ src/datasets/custom_text_dataset.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from src.datasets.base_dataset import BaseDataset
+
+
+class CustomDirTextDataset(BaseDataset):
+    def __init__(self, data_dir: str = None, text: str = None, *args, **kwargs):
+        if text:
+            data = [{"text": text}]
+        elif data_dir:
+            transcription_path = Path(data_dir) / "transcriptions"
+            data = [
+                {"path": str(path), "text": path.read_text(encoding="utf-8").strip()}
+                for path in transcription_path.iterdir()
+                if path.suffix.lower() == ".txt" and path.read_text(encoding="utf-8").strip()
+            ]
+        else:
+            data = []
+
+        super().__init__(data, *args, **kwargs)
