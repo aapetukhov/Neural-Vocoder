50324f412dcc14232220dc229b26615ce4b7356f
aapetukhov <andreybsongs@gmail.com>
2024-12-08 05:37:17 +0300

    update reqs

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,4 +1,5 @@
 from torch.nn.utils.rnn import pad_sequence
+import chain
 
 PAD_CONST = -11.51292
 
@@ -22,13 +23,7 @@ def collate_fn(dataset_items: list[dict]):
     audios = [audio for item in dataset_items for audio in item["audio"]]
     audios = pad_sequence(audios, batch_first=True)
 
-    spectrograms = []
-    for item in dataset_items:
-        spectrogram = item["spectrogram"]
-        if spectrogram.ndim == 3:
-            spectrogram = spectrogram.squeeze(0)
-        spectrograms.append(spectrogram.transpose(1, 2))
-    spectrograms = pad_sequence(spectrograms, batch_first=True, padding_value=PAD_CONST).transpose(1, 2)
+    spectrograms = list(chain.from_iterable(item['spectrogram'].transpose(1, 2) for item in dataset_items))
     texts = sum((item["text"] for item in dataset_items), [])
 
     return {
